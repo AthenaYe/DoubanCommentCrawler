1f2bcac421390b5e0d4356d1786e6ff88f359f40
AthenaYe <athenaye.uw@gmail.com>
2014-12-26 23:01:14 +0800

    fix

--- doubanmovie/moviecom.py
+++ doubanmovie/moviecom.py
@@ -6,12 +6,13 @@ from pyquery import PyQuery as pq
 import re
 import json
 import os
+import traceback
 
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def comment(name, link, movieid):
+def getLink(link):
     movie = pq(url=link)
     aa = movie('a')
     CommentLink = None
@@ -20,61 +21,77 @@ def comment(name, link, movieid):
         if 'comment' in CommentLink:
             break
     CommentLink = config.Suffix + CommentLink
-#    CommentLink = 'http://m.douban.com/movie/subject/21318488/comments?page=1233'
-    moviec = pq(url=CommentLink)
+    print CommentLink
+    return CommentLink
+
+def makejson(CommentItems):
+    commentdict = {}
+    odd = 0
+    for lines in CommentItems:
+        if odd % 2 == 1:
+            tmp = pq(lines).text()
+            if odd % 100 == 0:
+                print odd
+        #    print tmp
+            whole = re.match('-(.*) \(([1-5])', tmp)
+            if not whole:
+                continue
+            user = whole.group(1)
+            star = int(whole.group(2))
+      #      if star in [1,5]:
+            commentdict['moviename'] = name
+            commentdict['movielink'] = link
+            commentdict['user'] = user
+            commentdict['rating'] = star
+            f.write(json.dumps(commentdict, encoding="UTF-8", ensure_ascii=False))
+            f.write('\n')
+            commentdict.clear()
+        else:
+            commentdict['comment'] = pq(lines).text()
+        odd += 1
+
+def comment(name, link, movieid):
+
+    moviec = pq(url=getLink(link))
     try:
         f = open(config.CommentDir+movieid, 'w')
     except:
         return
     print "crawling movie:"+name
-    while True:
-
-        commentdict = {}
-
-        Body = pq(moviec('div[class="list"]'))
-        Body.pop()
-        PageLoad = None
-        CommentItems = Body('span')
-        Page = Body('a')
-        if CommentItems.size() < 4:
-            break
-        CommentItems.pop()
-        odd = 0
-        for lines in CommentItems:
-            if odd % 2 == 1:
-                tmp = pq(lines).text()
-                if odd % 100 == 0:
-                    print odd
-            #    print tmp
-                whole = re.match('-(.*) \(([1-5])', tmp)
-                if not whole:
-                    continue
-                user = whole.group(1)
-                star = int(whole.group(2))
-          #      if star in [1,5]:
-                commentdict['moviename'] = name
-                commentdict['movielink'] = link
-                commentdict['user'] = user
-                commentdict['rating'] = star
-                f.write(json.dumps(commentdict, encoding="UTF-8", ensure_ascii=False))
-                f.write('\n')
-                commentdict.clear()
-            else:
-                commentdict['comment'] = pq(lines).text()
-            odd += 1
-        for lines in Page:
-#            print pq(lines).text()
-            if pq(lines).text() == u'下一页':
-                PageLoad = pq(lines).attr('href')
-                PageLoad = config.Suffix + PageLoad
-                moviec = pq(url=PageLoad)
+    countpage = 0
+    PageLoad = '' 
+    try:
+        while True:
+            countpage += 1
+            Body = pq(moviec('div[class="list"]'))
+            Body.pop()
+            PageLoad = None
+            CommentItems = Body('span')
+            Page = Body('a')
+            if CommentItems.size() < 4:
                 break
-        if PageLoad == None:
-            break
-    f.close()
-    os.system('chmod 444 '+ config.CommentDir+movieid)
+            CommentItems.pop()
+            makejson(CommentItems)
+
+            for lines in Page:
+    #            print pq(lines).text()
+                if pq(lines).text() == u'下一页':
+                    PageLoad = pq(lines).attr('href')
+                    PageLoad = config.Suffix + PageLoad
+                    moviec = pq(url=PageLoad)
+                    break
+            if PageLoad == None:
+                break
+        f.close()
+        os.system('chmod 444 '+ config.CommentDir+movieid)
+    except:
+        print "you mafan le!!"
+        print "page:"+ countpage 
+        print PageLoad
+        traceback.print_exc()
+        return
 
 if __name__ == '__main__':
-    comment(u'消失的爱人', 'http://m.douban.com/movie/subject/21318488/?session=55b6a7d1', 'ceshi')
+    comment(u'少年时代', 'http://m.douban.com/movie/subject/2209575/', 'ceshi')
 
 # vim: ts=4 sw=4 sts=4 expandtab
